233c1c0344a7c38ffb03d795f2f9e2f21f6f1d6b
Yusuf mustahan <yumustyology@gmail.com>
2024-10-16 23:35:40 +0100

    feat:search-bar-cancel-button

--- src/components/atoms/Header.tsx
+++ src/components/atoms/Header.tsx
@@ -1,32 +1,44 @@
-import React from 'react';
-import { View, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import React from "react";
+import { View, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { Fontisto, Ionicons } from "@expo/vector-icons";
 
 const Header: React.FC = () => {
   return (
     <View style={styles.header}>
       <TouchableOpacity>
-        <Image source={require('../../../assets/avatar-profile.png')} style={{height:38,width:38}} />
+        <Image
+          source={require("../../../assets/avatar-profile.png")}
+          style={{ height: 38, width: 38 }}
+        />
       </TouchableOpacity>
-      <Image source={require('../../../assets/logo-text-blue.png')} style={styles.logo} />
+      <Image
+        source={require("../../../assets/logo-text-blue.png")}
+        style={styles.logo}
+      />
       <TouchableOpacity style={styles.notification}>
-        <Ionicons name="notifications-outline" size={25} color="#2F50C1" />
+        <Fontisto name="bell" size={24} color="#2F50C1" />
       </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  header: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginTop:15, marginBottom: 20 },
-  logo: { width: 120, height: 20, resizeMode: 'contain' },
-  notification:{
-      backgroundColor:'#F4F2F8',
-      height: 40,
-      width: 40,
-      alignItems:'center',
-      borderRadius: 1000,
-      justifyContent: 'center'
-  }
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 15,
+    marginBottom: 20,
+  },
+  logo: { width: 120, height: 20, resizeMode: "contain" },
+  notification: {
+    backgroundColor: "#F4F2F8",
+    height: 40,
+    width: 40,
+    alignItems: "center",
+    borderRadius: 1000,
+    justifyContent: "center",
+  },
 });
 
 export default Header;
